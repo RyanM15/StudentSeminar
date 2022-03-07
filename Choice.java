58a2265f22bcd6bfc64de96f5d9f746dbdd66eb9
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-03-07 11:02:40 -0500

    Create Choice.java

---
+++ Choice.java
@@ -0,0 +1,50 @@
+public class Choice{
+	
+	public static void main(String[] args)
+	{
+		public int act1 = 0;
+		public int act2 = 0;
+		public int act3 = 0;
+		public int act4 = 0;
+		public int act5 = 0;
+		public int act6 = 0;
+		public int act7 = 0;
+		public int act8 = 0;
+		public int act9 = 0;
+		public int act10 = 0;
+		public int act11 = 0;
+		public int act12 = 0;
+		public int act13 = 0;
+		public int act14 = 0;
+		public int act15 = 0;
+		public int act16 = 0;
+		public int act17 = 0;
+		public int act18 = 0;
+		
+		ArrayList<Integer> choice = new ArrayList<Integer>();
+		ArrayList<Integer> choiceCounter = new ArrayList<Integer>();//add the counter to this instead of variabless
+		
+		for(int i = 1; i < size.choice(); i++)
+		{
+			if (choice.get(0) == i || choice.get(1) == i || choice.get(2) == i || choice.get(3) == i || choice.get(4) == i)
+			{
+				//need to figure out a way to add to the right counter
+			}
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+
+}
