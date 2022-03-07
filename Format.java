fcbc5a3bece14f362a54e413c550dd3692f58d62
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-03-07 10:35:33 -0500

    Update Format.java

--- Format.java
+++ Format.java
@@ -1,52 +1,35 @@
-
+/* This class is designed to create a format for the final product which will present a nice image
+of the six classes and the five timeslots. It uses a 2d ArrayList and for loops to populate and 
+print out the format */
 import java.util.ArrayList;
 
-public class Format{
-	public static void main(String[] args){ //prints out the board 
+public class Format{ 
+	public static void main(String[] args){ 
 		
 		ArrayList<ArrayList<Character>> board = new ArrayList<ArrayList<Character>>();
 		
 		
-		for(int i = 0; i<6; i++)
+		for(int i = 0; i<6; i++) //creates 6 "rooms"
 		{
 			board.add(new ArrayList<Character>());
 			
-			for(int x = 0; x < 5; x++){
-				if(i%2 == 0)
-				{
-					if (x<5)
-					{
-						board.get(i).add('\u2610');
-					}
-					
-					
-				}
+			for(int x = 0; x < 5; x++){ //creates 5 "timeslots"
 				
-				else
-				{
-					if (x<5)
-					{
-						board.get(i).add('\u2610');
-					}
-					
-				}
-									
-					
-				
+						board.get(i).add('\u2610'); //adds the box image
 				
 			}
 		}
 		
 		
-		System.out.print("\nSIX ROOMS BY FIVE TIME SLOTS");
+		System.out.print("\nSIX ROOMS BY FIVE TIME SLOTS"); //header for the board
 		
-		int counter = 0;
+		
 		System.out.println("");
 		for(int i = 0; i<board.size(); i++)
 		{
 			
 			
-				System.out.println(board.get(i));
+				System.out.println(board.get(i)); //prints out board
 		
 			
 		}
