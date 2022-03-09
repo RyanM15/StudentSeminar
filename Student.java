3d93c41f8f08cb39773b61cf6be6e216bd7686f1
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-03-09 10:06:36 -0500

    changed minute to rank

--- Student.java
+++ Student.java
@@ -1,34 +1,20 @@
-/* This class creates the student constructor to accept all needed info about the students
- * and their choices from the scraped excell sheet*/
 import java.util.ArrayList;
 
 public class Student{
-	
 	//assigns all needed variable for student info
 	public String email;
 	public String name; 
-	public int minute;
+	public int rank;
 	ArrayList<Integer> choice = new ArrayList<Integer>(); //creates an ArrayList to add the choices to
 	
-	public Student(String email, String name, ArrayList<Integer> choice, int minute){
+	public Student(String email, String name, ArrayList<Integer> choice, int rank){
 		this.email = email;
 		this.name = name;
 		this.choice = choice;
-		this.minute = minute;
+		this.rank = rank;
 	}
 	
 	public String toString(){ //allows variables to be printed
-		return ("email: " + email + ", name: " + name + ", choices: " + choice + ", minute: " + minute); 
-		
+		return ("email: " + email + ", name: " + name + ", choices: " + choice + ", rank: " + rank); 
 	}
-
-
-
-
-
-
-
-
-
-
 }
